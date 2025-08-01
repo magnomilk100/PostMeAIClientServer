b6e01d9aeb44499bf7fede148a140a91d86d974b
magnomilk100 <magnomilk@yahoo.com.br>
2025-08-01 14:25:34 +0200

    Setting everything for cookie management to get by issue with .filter(), test 11

--- server/auth.ts
+++ server/auth.ts
@@ -52,7 +52,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // secure only in prod
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     },
   })
